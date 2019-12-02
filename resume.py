71ad8a5725b0da6e8cff9314a10a280e6499cb9e
tomoshi0809 <ankou.research@gmail.com>
2019-12-02 23:08:06 +0900

    Change .bag file name

--- resume.py
+++ resume.py
@@ -7,7 +7,7 @@ CLIP_LWBOUND_METER = 0.3 # lower bound of clipping distance (m)
 
 pipeline = rs.pipeline()
 config = rs.config()
-config.enable_device_from_file('./records/201912012056.bag')
+config.enable_device_from_file('./records/201912012154.bag')
 config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
 config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
 config.enable_stream(rs.stream.infrared, 1, 640, 480, rs.format.y8, 30)
